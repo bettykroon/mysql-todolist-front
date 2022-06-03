77129f19078928ae335a0854fa73d68601c0a587
bettykroon <betty.kroon@hotmail.com>
2022-06-03 13:21:47 +0200

    multiple lists

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,26 +6,72 @@ import './Home.scss';
 export function Home(){
 
     const [ todos, setTodos ] = useState([]);
-    
+    const [ lists, setLists ] = useState([]);
+
+    let list: any = localStorage.getItem("list") || null;
+    localStorage.setItem("list", list);
+
     useEffect(() => {
+        console.log(list);
+        
+        if(list === "null"){
+            console.log("NULL");
+        } else {
+            console.log(list);
+            getList(list);
+        }
+        
         fetch("http://localhost:3000/users")
         .then(res => res.json())
         .then(data => {
+            console.log(data);
             for (let i = 0; i < data.length; i++) {
-                setTodos(data)              
+                setLists(data);                
             }
         })
-    }, []);
+    }, [list])
     
     const [ todo, setTodo ] = useState([]);
+    const [ emptyList, setEmptyList ] = useState("");
+
+    const todoObject = Object.values(todo);
 
     function handleChange(e: ChangeEvent<HTMLInputElement>){
         let name = e.target.name;
         setTodo({...todo, [name]: e.target.value});
     }
 
-    function save(){
-        console.log(todo);
+    const [ listBtn, setListBtn ] = useState(false);
+    function getList(list: any){
+        setListBtn(true);
+        let getList = [{list: list}];
+        localStorage.setItem("list", list);
+        fetch("http://localhost:3000/users/list", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(getList)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.length === 0){
+                let emptyList = "Listan är tom..";
+                setEmptyList(emptyList);
+                console.log("LIST", list);
+                setTodos([]);
+            }
+            for (let i = 0; i < data.length; i++) {
+                setTodos(data);
+                setEmptyList("");           
+            }
+        })
+    }
+
+    function save(list: any){
+        let saveToList = [{list: list}, todo];
+        console.log(saveToList);
         
         fetch("http://localhost:3000/users", {
             method: "POST",
@@ -33,7 +79,7 @@ export function Home(){
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
             },
-            body: JSON.stringify(todo)
+            body: JSON.stringify(saveToList)
         })
         .then(res => res.json())
         .then(data => {
@@ -43,8 +89,8 @@ export function Home(){
         window.location.reload();
     }
 
-    function done(delTodo: any){
-        let todoToDelete = [{todo: delTodo}];
+    function done(list: any, delTodo: any){
+        let todoToDelete = [{list: list}, {todo: delTodo}];
         
         fetch('http://localhost:3000/users/delete', {
             method: "POST",
@@ -65,23 +111,58 @@ export function Home(){
     let lis = todos.map((todo, i) => {
         return (
         <li key={i}>
-            <h3>{todo} <FontAwesomeIcon className="icon" icon={faCircleXmark} onClick={() => done(todo)}></FontAwesomeIcon></h3>
+            <h3>{todo} <FontAwesomeIcon className="icon" icon={faCircleXmark} onClick={() => done(list, todo)}></FontAwesomeIcon></h3>
         </li>)
     })
 
+    let allLists = lists.map((list, i) => {
+        return (
+            <button key={i} onClick={() => getList(list)}>{list}</button>
+        )
+    })
+
+    const [ newListBtn, setNewListBtn ] = useState(false);
+    function newList(){
+        fetch("http://localhost:3000/users/add", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(todo)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+        getList(todoObject[0]);
+        window.location.reload();
+    }
+
     return (<>
         <header>
-            <h1>My todolist</h1>
+            <h1>Listify</h1>
         </header>
 
         <div className="todos">
-            <form>
-                <input type="text" name="todo" placeholder="Todo" onChange={handleChange}/>
-                <button type="button" onClick={save}>Save</button>
+            <h2>My lists</h2>
+            {allLists}
+
+            <button onClick={() => setNewListBtn(true)}>+ New list</button>
+            {listBtn && !newListBtn && <><form>
+                <input type="text" name="todo" placeholder={`add to ${list}`} onChange={handleChange}/>
+                <button type="button" onClick={() => save(list)}>Add</button>
             </form>
 
-            <h2>Todos:</h2>
+            <h2>{list}:</h2>
             <ul>{lis}</ul>
+            {emptyList}</>}
+
+            {newListBtn && <>
+                <form>
+                    <input type="text" name="list" placeholder="Name" onChange={handleChange}/>
+                    <button type="button" onClick={newList}>Create</button>
+                </form></>}
         </div>
     </>)
 }
